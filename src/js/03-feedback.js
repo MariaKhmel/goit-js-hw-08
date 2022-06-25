59a5db33899101eefb89c097ca723ca16c308663
MariaKhmel <mariakhmel8@gmail.com>
2022-06-25 21:28:45 +0300

    3rd task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
 
+const STORAGE_KEY = 'feedback-form-state';
+
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onFormInput, 500));
+
+
+
+const emailItem = refs.form.elements.email;
+const messageItem = refs.form.elements.message;
+
+populateTextarea();
+
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+ evt.currentTarget.reset();
+console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+
+function onFormInput(evt) {
+  const email = emailItem.value;
+    const message = messageItem.value;
+localStorage.setItem(STORAGE_KEY, JSON.stringify({ email, message }));
+
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    const formData = JSON.parse(savedMessage);
+    emailItem.value = formData.email;
+    messageItem.value = formData.message;
+  }
+}
