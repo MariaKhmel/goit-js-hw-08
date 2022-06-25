17ca81ff79489fa78aa66e6eff4b8818b898baad
MariaKhmel <mariakhmel8@gmail.com>
2022-06-25 06:16:30 +0300

    updates

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+ const iframe = document.querySelector('iframe');
+ const player = new Player(iframe);
+player.on(
+    'timeupdate',
+    throttle(function (data) {
+      localStorage.setItem('videoplayer-current-time', data.seconds);
+    }, 1000)
+);
+  
+  const data = localStorage.getItem('videoplayer-current-time');
+
+  player.setCurrentTime(data);
