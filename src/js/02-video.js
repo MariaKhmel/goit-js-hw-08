50c4f97ffcd38079b5e82efcac48e8013da70c0e
MariaKhmel <mariakhmel8@gmail.com>
2022-06-25 20:16:58 +0300

    video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,13 +2,17 @@ import Player from '@vimeo/player';
 const throttle = require('lodash.throttle');
 
  const iframe = document.querySelector('iframe');
- const player = new Player(iframe);
-player.on(
-    'timeupdate',
-    throttle(function (data) {
-      localStorage.setItem('videoplayer-current-time', data.seconds);
-    }, 1000)
-);
+const player = new Player(iframe);
+ 
+
+player.on('timeupdate', throttle(onPlay, 1000));
+    
+
+
+function onPlay (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
   
   const data = localStorage.getItem('videoplayer-current-time');
 
